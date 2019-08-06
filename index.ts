7b0119cff66f7822270e4c1cea7629e0241a2811
Chris <7539871+paxperscientiam@users.noreply.github.com>
2019-08-06 17:52:00 -0400

    rename key constant to accessor

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
-export default function dlv(obj: object, key: string|string[], def?: any, p?: number): any {
-    p = 0
-    key = (key as string).split ? (key as string).split(".") : (key as string[])
-    while (obj && p < key.length) {
-        obj = obj[key[p++]]
+export default function dlv(obj: object, key: string|string[], def?: any, p: number = 0): any {
+    const accessor = (key as string).split ? (key as string).split(".") : (key as string[])
+    while (obj && p < accessor.length) {
+        obj = obj[accessor[p++]]
     }
-    return (obj === undefined || p < key.length) ? def : obj
+    return (obj === undefined || p < accessor.length) ? def : obj
 }
