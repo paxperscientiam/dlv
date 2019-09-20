e9c478d75f87d8910ea29348aa43018816e86b38
Chris <7539871+paxperscientiam@users.noreply.github.com>
2019-09-20 17:07:09 -0400

    finally figured out how to prevent null fallback value

--- index.ts
+++ index.ts
@@ -1,10 +1,15 @@
-export default function dlv<T, K extends Extract<keyof T, string>>(obj: T,
-                                                                   key: K|K[],
-                                                                   def?: any,
-                                                                   p: number = 0): any {
+// https://www.typescriptlang.org/docs/handbook/utility-types.html#nonnullablet
+
+export default function dlv<T, K extends Extract<keyof T, string>, L>(obj: T,
+                                                                      key: K|K[],
+                                                                      fallback?: NonNullable<L>,
+                                                                      p: number = 0): L|T {
     const accessor = (key as string).split ? (key as string).split(".") : (key as string[])
     while (obj && p < accessor.length) {
         obj = obj[accessor[p++]]
     }
-    return (obj === undefined || p < accessor.length) ? def : obj
+
+    return (obj === undefined || p < accessor!.length) ? fallback as L : obj as T
 }
+
+// dlv({a: 3, b: true, n: 4, g: {f: true}}, "a", null) // disallow null fallback
